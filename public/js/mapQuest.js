73f3996f597cca11bd1c34bd0c77e2cd8bad8268
Sophia <sodoming@ucsd.edu>
2020-02-27 11:38:59 -0800

    adding

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'WaY2n1yLpWZqCH8jiNadRL4f0EgzGF6V';
+
+	var map = L.mapquest.map('map',{
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 15,
+		zoomControl: false
+	});
+
+	L.marker([32.879003,-117.236027]).addTo(map);
 }
